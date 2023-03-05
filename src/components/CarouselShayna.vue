a4fe5241dc7071488170e70d28f019f6b188b9f3
Taopik Rahman <1806078@itg.ac.id>
2023-03-05 15:03:20 +0700

    menambahkan carousel store

---
+++ src/components/CarouselShayna.vue
@@ -0,0 +1,63 @@
+<template>
+    <section class="hero-section">
+        <template slot="prev"><span class="prev">prev</span></template>
+        <carousel class="hero-items" :items="1" :autoplay="true" :nav="false">
+            <div class="single-hero-items set-bg" style="background-image: url('/img/hero-1.jpg');">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <span>Bag,kids</span>
+                            <h1>Black friday</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-hero-items set-bg" style="background-image: url('/img/hero-2.jpg');">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <span>Bag,kids</span>
+                            <h1>Black friday</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-hero-items set-bg" style="background-image: url('/img/hero-1.jpg');">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <span>Bag,kids</span>
+                            <h1>Black friday</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+        </carousel>
+        <template slot="next"><span class="next">next</span></template>
+    </section>
+</template>
+
+<script>
+
+import carousel from 'vue-owl-carousel';
+
+export default {
+    name: "CarouselShayna",
+    components: {
+        carousel
+    }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <CarouselShayna />
+    <WomanShayna />
+    <InstagramShayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+// import CarouselShayna from "@/components/CarouselShayna.vue";
+import CarouselShayna from "@/components/CarouselShayna.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import WomanShayna from '@/components/WomanShayna.vue';
+import InstagramShayna from '@/components/InstagramShayna.vue';
+
+
+
+export default {
+  name: "HomeView",
+  components: {
+    // HelloWorld
+    HeaderShayna,    
+    CarouselShayna,
+    WomanShayna,
+    InstagramShayna
+},
+};
+</script>
